ce458837d30c7e47a332236878297fd29b91da2e
peterlubrich <64193173+peterlubrich@users.noreply.github.com>
2025-01-17 15:45:19 +0100

    Update config.js

--- drafts/latest/config.js
+++ drafts/latest/config.js
@@ -32,7 +32,7 @@ var respecConfig = {
 //    copyrightHolder: "European Union",
 //    copyrightURL: "https://europa.eu/",
     copyrightStart: 2023,
-    publishDate: "2024-12-23",
+    publishDate: "2025-01-17",
 
     alternateFormats: [
 /*
