ae0aa96599f06e64fd825af09590fbbfcf7823ab
Daham-Mustaf <daham.mhamad1@gmail.com>
2026-04-03 13:02:58 +0200

    fix(config): update SHACL alternateFormats to three-file structure

--- drafts/1.1.0-draft-0.1/config.js
+++ drafts/1.1.0-draft-0.1/config.js
@@ -62,8 +62,16 @@ var respecConfig = {
       uri: "./mobilitydcat-ap.jsonld",
     },
     {
-       label: "SHACL (Turtle)",
-       uri: "./validationFiles/mobilitydcat-ap_shacl_shapes.ttl",
+      label: "SHACL Basic Validation (Turtle)",
+      uri: "./shaclShapes/mobilitydcat-ap_1.1.0_shacl_shapes.ttl",
+    },
+    {
+      label: "SHACL Range Constraints (Turtle)",
+      uri: "./shaclShapes/mobilitydcat-ap_1.1.0_shacl_range.ttl",
+    },
+    {
+      label: "SHACL Controlled Vocabulary Constraints (Turtle)",
+      uri: "./shaclShapes/mobilitydcat-ap_1.1.0_shacl_mdr-vocabularies.shape.ttl",
     }
     ],
 //    implementationReportURI:"https://joinup.ec.europa.eu/document/geodcat-ap-implementations",
