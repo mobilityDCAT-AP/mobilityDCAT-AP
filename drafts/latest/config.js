4bb0d7cb816a17c90e5da68112b50a302ef21099
peterlubrich <64193173+peterlubrich@users.noreply.github.com>
2026-04-09 17:27:36 +0200

    Update logo URL

--- drafts/latest/config.js
+++ drafts/latest/config.js
@@ -23,7 +23,7 @@ var respecConfig = {
     subtitle: "A mobility extension for the DCAT application profile for data portals in Europe",
     logos: [
         {
-        src: 'https://github.com/mobilityDCAT-AP/mobilityDCAT-AP/blob/gh-pages/drafts/latest/figures/mobilityDCAT-AP-V3-logo.png',
+        src: 'https://mobilitydcat-ap.github.io/respec-style/logos/mobilityDCAT-AP-V3-logo.png',
         width: 250,
         height: 140,
         alt: "mobilitydcat-ap logo"
